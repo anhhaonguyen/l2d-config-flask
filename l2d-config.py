7e6ec04e5598309fa60931bb6f6308af13a2bafa
anhhaonguyen <haonguyen1008@gmail.com>
2016-11-22 22:03:31 +0700

    server done

--- l2d-config.py
+++ l2d-config.py
@@ -1,6 +1,7 @@
 from numpy import *
 from scipy.optimize import leastsq
 import math, json, socket
+import os, errno
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -13,10 +14,40 @@ def index():
 # ================================================================================= 
 @app.route('/create', methods=['POST'])
 def create():
-	return 'create ok'
+    data = request.json
+    evo_name = data['name']
+    config_file = evo_name + '.json'
+    with open(config_file, 'w') as output:
+        json.dump(data['config'], output, indent=4, sort_keys=True, separators=(',', ':'))
+	return 'create done'
 # ================================================================================= 
 @app.route('/update', methods=['POST'])
 def update():
-	return 'update ok'
+    data = request.json
+    evo_name = data['name']
+    config_file = evo_name + '.json'
+    with open(config_file, 'w') as output:
+        json.dump(data['config'], output, indent=4, sort_keys=True, separators=(',', ':'))
+	return 'update done'
+# ================================================================================= 
+@app.route('/config', methods=['POST'])
+def config():
+    data = request.json
+    evo_name = data['name']
+    config_file = evo_name + '.json'
+    with open(config_file) as json_data:
+        d = json.load(json_data)
+        return json.dumps(d)
+# ================================================================================= 
+@app.route('/delete', methods=['POST'])
+def delete():
+    data = request.json
+    evo_name = data['name']
+    config_file = evo_name + '.json'
+    try:
+        os.remove(config_file)
+        return 'delete done'
+    except OSError:
+        return 'file not exist'
 # =================================================================================
 app.run(host='localhost', port=5000)
